a67731d1434de8ad9e883818eacc46eeb36eae01
Tomas Miranda <tomas.miranda@alu.ing.unlp.edu.ar>
2024-11-04 14:28:39 -0300

    credicoop sample branch
    This branch aims to focus the script's structure to automate conversion ofCredicoop bank pdfs. This will be part of the main branch as a sample feature in a future release.

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
-import sys, tabula, pandas
+import sys, tabula, pandas, os
+directory = os.getcwd()
+slash = '\\'
 default = 51
 
 if len(sys.argv) > 1:
     pdf_name = sys.argv[1]
-    path = fr'C:\Users\RTX\Desktop\python\pdftocsv\pdf\{pdf_name}.pdf'
+    path = fr'pdf\{pdf_name}.pdf'
+    path = slash.join([directory, path])
     if len(sys.argv) > 3:
         csv_name = sys.argv[2]
         last_page = int(sys.argv[3])+1
         if len(sys.argv) > 4:
             last_page_length = int(sys.argv[4])
+            if len(sys.argv) > 5:
+                first_page_length = int(sys.argv[5])
+            else:
+                first_page_length = 27
+        else:
+            last_page_length = default
     else:
         csv_name = pdf_name
+        last_page_length = default
         if len(sys.argv) == 2:
             last_page = 15
         else:
             last_page = int(sys.argv[2])+1
-        last_page_length = default
-    output_path = fr'C:\Users\RTX\Desktop\python\pdftocsv\csv\{csv_name}.csv'
+    output_path = fr'csv\{csv_name}.csv'
+    output_path = slash.join([directory, output_path])
 else:
-    path = r'C:\Users\RTX\Desktop\python\pdftocsv\pdf\test.pdf'
-    output_path = r'C:\Users\RTX\Desktop\python\pdftocsv\csv\test.csv'
-    last_page = 15
+    path = r'pdf\credicoop\test.pdf'
+    output_path = r'csv\credicoop.csv'
+    path = slash.join([directory, path])
+    output_path = slash.join([directory, output_path])
+    first_page_length = 35
     last_page_length = default
+    last_page = 15
 
 def page_call(i):
-    if i == (last_page-1):
-        page = tabula.read_pdf(path, pages=i, area=[23,0,last_page_length,100], relative_area=True, relative_columns=True, columns=[11,17,55,67,85,100])
+    if i == 1:
+        page = tabula.read_pdf(path, pages=i, pandas_options={'header': None}, area=[first_page_length,0,91,100], relative_area=True, relative_columns=True, columns=[11,17,55,67,85,100])
+    elif i == (last_page-1):
+        page = tabula.read_pdf(path, pages=i, pandas_options={'header': None}, area=[24,0,last_page_length,100], relative_area=True, relative_columns=True, columns=[11,17,55,67,85,100])
     else:
-        page = tabula.read_pdf(path, pages=i, area=[23,0,92,100], relative_area=True, relative_columns=True, columns=[11,17,55,67,85,100])
+        page = tabula.read_pdf(path, pages=i, pandas_options={'header': None}, area=[24,0,92,100], relative_area=True, relative_columns=True, columns=[11,17,55,67,85,100])
     page[0].columns = ['Fecha','ID','Descripcion','Debito','Credito','Saldo']
     return page
 
@@ -44,7 +59,7 @@ def update(DATA,page):
     DATA['Saldo'].extend(page['Saldo'])
     return DATA
 
-def add_null_raw(DATA):
+def add_null_row(DATA):
     raw = [None]
     row = {}
     columns = ['Fecha','ID','Descripcion','Debito','Credito','Saldo']
@@ -65,16 +80,14 @@ def data_check(DATA):
 
 for i in range(1,last_page):
     if i == 1:
-        df = tabula.read_pdf(path, pages='1', pandas_options={'header': None}, area=[35,0,91,100], relative_area=True, relative_columns=True, columns=[11,17,55,67,85,100])
-        df[0].columns = ['Fecha','ID','Descripcion','Debito','Credito','Saldo']
-        DATA = to_list(df)
-        add_null_raw(DATA)
+        page = page_call(i)
+        DATA = to_list(page)
+        add_null_row(DATA)
     else:
         page = page_call(i)
         add = to_list(page)
         DATA = update(DATA,add)
-        add_null_raw(DATA)
+        add_null_row(DATA)
         if i == (last_page-1):
             dataframe = pandas.DataFrame.from_dict(DATA)
             dataframe.to_csv(path_or_buf=output_path, decimal=',')
-
